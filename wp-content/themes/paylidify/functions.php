da003281bde36ded5cb0c124f90a8a9e0531d08f
Tami Mitchell <mail@tameramitchell.com>
2019-02-10 10:55:51 -0700

    Add Font Awesome 5

--- wp-content/themes/paylidify/functions.php
+++ wp-content/themes/paylidify/functions.php
@@ -126,6 +126,8 @@ function paylidify_scripts() {
 
 	wp_enqueue_script( 'paylidify-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_style( 'load-fa', 'https://pro.fontawesome.com/releases/v5.7.1/css/all.css' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -158,4 +160,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
