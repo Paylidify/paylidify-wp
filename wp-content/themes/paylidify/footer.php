b0881ea8b49ca3f9d07e4ce2b0a28ce405767eb3
Tami Mitchell <mail@tameramitchell.com>
2019-02-20 19:49:36 -0600

    Fix overflow issue

--- wp-content/themes/paylidify/footer.php
+++ wp-content/themes/paylidify/footer.php
@@ -18,60 +18,61 @@ if ( $in_canada ) {
 }
 ?>
 
-	</div><!-- #content -->
-	<footer id="colophon" class="site-footer">
-		<div class="elementor-row">
-			<div class="footer-logo">
-				<?php the_custom_logo(); ?>
+		</div><!-- #content -->
+		<footer id="colophon" class="site-footer">
+			<div class="elementor-row">
+				<div class="footer-logo">
+					<?php the_custom_logo(); ?>
+				</div>
+
+				<nav id="footer-navigation" class="footer-navigation">
+					<?php
+					// Show CA menu if on CA-EN version
+					if ($in_canada) {
+						wp_nav_menu( array(
+							'theme_location' => 'footer-menu-ca',
+							'menu_id'        => 'footer-menu',
+						) );
+					} else {
+						wp_nav_menu( array(
+							'theme_location' => 'footer-menu-us',
+							'menu_id'        => 'footer-menu',
+						) );
+					}
+					?>
+				</nav>
 			</div>
 
-			<nav id="footer-navigation" class="footer-navigation">
-				<?php
-				// Show CA menu if on CA-EN version
-				if ($in_canada) {
-					wp_nav_menu( array(
-						'theme_location' => 'footer-menu-ca',
-						'menu_id'        => 'footer-menu',
-					) );
-				} else {
-					wp_nav_menu( array(
-						'theme_location' => 'footer-menu-us',
-						'menu_id'        => 'footer-menu',
-					) );
-				}
-				?>
-			</nav>
-		</div>
+			<div class="elementor-row">
+				<div class="elementor-column elementor-col-33">
+					<div class="country-flag country-flag-<?php echo $country_flag_class; ?>"></div>
 
-		<div class="elementor-row">
-			<div class="elementor-column elementor-col-33">
-				<div class="country-flag country-flag-<?php echo $country_flag_class; ?>"></div>
+					<?php if ( is_active_sidebar( 'footer-left' ) ) { ?>
+						<div id="footer-widget" class="widget-area">
+							<?php dynamic_sidebar( 'footer-left' ); ?>
+						</div>
+					<?php } ?>
+				</div>
 
-				<?php if ( is_active_sidebar( 'footer-left' ) ) { ?>
-					<div id="footer-widget" class="widget-area">
-						<?php dynamic_sidebar( 'footer-left' ); ?>
-					</div>
-				<?php } ?>
+				<div class="elementor-column elementor-col-33">
+					<?php if ( is_active_sidebar( 'footer-center' ) ) { ?>
+						<div id="footer-widget" class="widget-area">
+							<?php dynamic_sidebar( 'footer-center' ); ?>
+						</div>
+					<?php } ?>
+				</div>
+
+				<div class="elementor-column elementor-col-33">
+					<?php if ( is_active_sidebar( 'footer-right' ) ) { ?>
+						<div id="footer-widget" class="widget-area">
+							<?php dynamic_sidebar( 'footer-right' ); ?>
+						</div>
+					<?php } ?>
+				</div>
 			</div>
-
-			<div class="elementor-column elementor-col-33">
-				<?php if ( is_active_sidebar( 'footer-center' ) ) { ?>
-					<div id="footer-widget" class="widget-area">
-						<?php dynamic_sidebar( 'footer-center' ); ?>
-					</div>
-				<?php } ?>
-			</div>
-
-			<div class="elementor-column elementor-col-33">
-				<?php if ( is_active_sidebar( 'footer-right' ) ) { ?>
-					<div id="footer-widget" class="widget-area">
-						<?php dynamic_sidebar( 'footer-right' ); ?>
-					</div>
-				<?php } ?>
-			</div>
-		</div>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+</div><!-- #page-wrap -->
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/paylidify/header.php
+++ wp-content/themes/paylidify/header.php
@@ -22,56 +22,57 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'paylidify' ); ?></a>
+<div id="page-wrap">
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'paylidify' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-
-			// show white logo for reseller page
-
-			if (strpos($current_url, 'reseller-partners') !== false) { ?>
-				<a href="/" class="custom-logo-link" rel="home" itemprop="url">
-					<img width="204" height="69" src="/wp-content/uploads/2019/02/paylidify-logo-white.png" class="custom-logo" alt="Paylidify" itemprop="logo">
-				</a>
-			<?php } else {
-				the_custom_logo();
-			}
-
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$paylidify_description = get_bloginfo( 'description', 'display' );
-			if ( $paylidify_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $paylidify_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'paylidify' ); ?></button>
-			<?php
-			// Show CA menu if on CA-EN version
-			if (strpos($current_url, 'ca-en') !== false) {
-				wp_nav_menu( array(
-					'theme_location' => 'header-menu-ca',
-					'menu_id'        => 'primary-menu',
-				) );
-			} else {
-				wp_nav_menu( array(
-					'theme_location' => 'header-menu-us',
-					'menu_id'        => 'primary-menu',
-				) );
-			}
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				// show white logo for reseller page
+
+				if (strpos($current_url, 'reseller-partners') !== false) { ?>
+					<a href="/" class="custom-logo-link" rel="home" itemprop="url">
+						<img width="204" height="69" src="/wp-content/uploads/2019/02/paylidify-logo-white.png" class="custom-logo" alt="Paylidify" itemprop="logo">
+					</a>
+				<?php } else {
+					the_custom_logo();
+				}
+
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$paylidify_description = get_bloginfo( 'description', 'display' );
+				if ( $paylidify_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $paylidify_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'paylidify' ); ?></button>
+				<?php
+				// Show CA menu if on CA-EN version
+				if (strpos($current_url, 'ca-en') !== false) {
+					wp_nav_menu( array(
+						'theme_location' => 'header-menu-ca',
+						'menu_id'        => 'primary-menu',
+					) );
+				} else {
+					wp_nav_menu( array(
+						'theme_location' => 'header-menu-us',
+						'menu_id'        => 'primary-menu',
+					) );
+				}
+				?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
